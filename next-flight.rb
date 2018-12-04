60dee02c43305640fa46a43e07c762317a1005de
Meder <m.toktosunov@icloud.com>
2018-12-03 23:34:00 -0500

    completed part 2

--- next-flight.rb
+++ next-flight.rb
@@ -1,10 +1,50 @@
 # YOUR CODE HERE
-flights = [2,5,7.5,8.5,9,10,11.5,13.5,14.5,17,18,19,24]
+require 'pry'
+flights = [2,5,13,7.5,8.5,9,10,11.5,13.5,14.5,17,18,19,24]
+lyrics = """
+***DON'T STOP...BELIEVIN'!***
 
-puts "What time are you leaving?"
+Just a small town girl
+Living in a lonely world
+She took the midnight train going anywhere
+Just a city boy
+Born and raised in South Detroit
+He took the midnight train going anywhere
 
-time = 12.5
+A singer in a smoky room
+A smell of wine and cheap perfume
+For a smile they can share the night
+It goes on and on and on and on
 
-flights.each do
-  
+Strangers waiting, up and down the boulevard
+Their shadows searching in the night
+Streetlights people, living just to find emotion
+Hiding, somewhere in the night.
+"""
+
+time = 0
+
+loop do
+  print "What time are you leaving?: "
+  time = gets.chomp.to_i
+  # binding.pry
+  if time != 0
+    break
+  else
+    puts "I do not understand that time!"
+    puts
+  end
+end
+
+flights.sort!
+
+flight_num = 0
+
+flights.each do |flight|
+  flight_num += 1
+  if flight > time
+    puts "Yout should catch Flight #{flight_num} leaving at #{flight}"
+    puts lyrics if flight == 24
+    break
+  end
 end
---
+++ spec/next-flight_spec.rb
@@ -0,0 +1,8 @@
+
+# require_relative "../next-flight.rb"
+#
+# RSpec.describe "../next-flight.rb" do
+#   it "should return" do
+#     expect(next-flight.rb).to output("hey").to_stdout
+#   end
+# end
